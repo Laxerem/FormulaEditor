334884f1b1f9da7edb4eeee1e22a510363ab99c5
Grigory Vorobev <grishaMas@yandex.ru>
2023-07-21 00:51:30 +0700

    Теперь можно добавить формулу, после чего ввести значения в переменные, ввести формулы, которые необходимо найти, и ты получаешь ответ, полученный на основе формул

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from formulas import get_arrs, get_itog_formulus, forms, value, naity, itog_form, vozm_form
+from formulas import get_arrs, get_itog_formulus, get_otvet, forms, value, naity, itog_form, vozm_form, otvet_dict, operato, dict
 
 
 print("Введите количество формул: ", end='')
@@ -15,10 +15,18 @@ for i in range(col_naity):
     get_naity = input()
     naity.append(get_naity)
 get_itog_formulus()
+get_otvet()
 
 print(forms)
 print(value)
 print(naity)
 print(vozm_form)
 print(itog_form)
+print(operato)
+print(dict)
+print(otvet_dict)
+keys_list = list(otvet_dict.keys())
+for i in range(len(keys_list)):
+    print("Ответ:", keys_list[i], "=", otvet_dict[keys_list[i]])
+
 
--- formulas.py
+++ formulas.py
@@ -1,10 +1,21 @@
+import operator
 forms = []
 value = []
 naity = []
 itog_form = []
 vozm_form = []
-formulus = ["C = p * b", "C = u / b", "N = i * m"]
-char = str
+formulus = ["C = p * b", "C = u / b", "N = t * y"]
+operato = []
+operators = ["+", "-", "*", "/"]
+operator_dict = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+    "/": operator.truediv
+}
+flag_forms = []
+dict = {}
+otvet_dict = {}
 
 
 def get_arrs(povtor):
@@ -46,13 +57,36 @@ def get_itog_formulus():
             if char == "=":
                 b = True
             if b == True:
+                if char in operators:
+                     operato.append(char)
                 if char.isalpha() and char in forms:
                     continue
-                elif char.isalpha() and char not in forms:
+                if char.isalpha() and char not in forms:
                     a = False
                     break
         if a == True:
             itog_form.append(string)
     return itog_form
 
+
+
+def get_dict():
+    for i in range(len(forms)):
+        dict[forms[i]] = value[i]
+    return dict
+
+def get_otvet():
+    get_dict()
+    j = False
+    index = 0
+    for string in itog_form:
+        for char in string:
+          if char == "=":
+               j = True
+          if char.isalpha() and j == True:
+               flag_forms.append(char)
+        otvet_dict[naity[index]] = operator_dict[operato[index]](dict[flag_forms[index]], dict[flag_forms[index + 1]])
+        flag_forms.clear()
+        index += 1
+    return otvet_dict
 
